ca8af9c2636cf4acc19bd5a53be6c1aac986c250
unknown <shajeb17.11@gmail.com>
2025-07-04 19:53:59 +0600

    created new file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let clickChange=document.querySelector(".clickChange");
+clickChange.addEventListener("click",function(){
+    let widthChange=document.querySelector(".widthChange");
+    widthChange.classList.toggle("newChange")
+})
+
