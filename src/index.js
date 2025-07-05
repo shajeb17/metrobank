fb146c57f7a7845e893b50e1ac9f6aba447c9d02
unknown <shajeb17.11@gmail.com>
2025-07-05 12:58:20 +0600

    head compleate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-let clickChange=document.querySelector(".clickChange");
-clickChange.addEventListener("click",function(){
-    let widthChange=document.querySelector(".widthChange");
+let clickChange = document.querySelector(".clickChange");
+clickChange.addEventListener("click", function () {
+    let widthChange = document.querySelector(".widthChange");
     widthChange.classList.toggle("newChange")
 })
 
+let asdcolor = document.querySelectorAll(".asdcolor");
+asdcolor.forEach(singlecolor => {
+    singlecolor.addEventListener("click", function () {
+        let coll = singlecolor.getAttribute("data-color")
+        document.body.style.setProperty("--theme-color", coll)
+        localStorage.setItem("coloradd", coll)
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    let get = localStorage.getItem("coloradd")
+    if (get) {
+        document.body.style.setProperty("--theme-color", get)
+    }
+})
